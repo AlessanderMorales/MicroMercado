d9dfe0796a3548a86615590158f0456c68b693ca
JaimeTadeo <deugartetadeo@gmail.com>
2025-10-09 00:29:45 -0400

    merge de main a pruebas

---
+++ prueba2/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Moq;
+using Xunit;
+using MicroMercado.Services;
+
+namespace prueba2
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
