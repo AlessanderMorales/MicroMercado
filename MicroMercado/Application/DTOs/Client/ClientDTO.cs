e5940b1a14bcdd8879f4cb278392999d5c545fda
christian-ledezma <chris.ledezma.s@gmail.com>
2025-10-15 11:08:16 -0400

    Refactor: refactor DTO client

--- MicroMercado/Application/DTOs/Client/ClientDTO.cs
+++ MicroMercado/Application/DTOs/Client/ClientDTO.cs
@@ -3,9 +3,9 @@ namespace MicroMercado.Application.DTOs.Client;
 public class ClientDTO
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string FullName => $"{Name} {LastName}";
+    public string BusinessName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
     public string TaxDocument { get; set; } = string.Empty;
     public byte Status { get; set; }
     public string StatusText => Status == 1 ? "Activo" : "Inactivo";
