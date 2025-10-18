a6b7d73290cc3f7e6cdb699d7c35e99f45f89924
AlessanderMorales <alessmoralesmo@gmail.com>
2025-10-18 14:05:35 -0400

    Fix de Models

--- MicroMercado/Domain/Models/Client.cs
+++ MicroMercado/Domain/Models/Client.cs
@@ -4,7 +4,7 @@ public class Client
 {
     public int Id { get; set; }
     public string BusinessName { get; set; } = string.Empty; 
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
     public string Address { get; set; } 
     public string TaxDocument { get; set; } = string.Empty;
     public byte Status { get; set; } = 1;
