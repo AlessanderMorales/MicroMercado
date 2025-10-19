b0a5ec036f82d1e994a23721ea99d1d786fd8540
christian-ledezma <chris.ledezma.s@gmail.com>
2025-10-19 00:06:27 -0400

    Feat: create product DTO and validator

---
+++ MicroMercado/Application/DTOs/Product/CreateProductDTO.cs
@@ -0,0 +1,13 @@
+namespace MicroMercado.Application.DTOs.Product;
+
+public class CreateProductDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public short Stock { get; set; }
+    public byte CategoryId { get; set; }
+    public string CategoryName { get; set; } = string.Empty;
+
+}
