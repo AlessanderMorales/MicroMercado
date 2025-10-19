d885bd4a7b6e7be06f49226c0011be4b723989b7
christian-ledezma <chris.ledezma.s@gmail.com>
2025-10-19 00:19:17 -0400

    Feat: create product Service Interface

--- MicroMercado/Application/Services/IProductService.cs
+++ MicroMercado/Application/Services/IProductService.cs
@@ -1,4 +1,6 @@
+using MicroMercado.Application.DTOs.Product;
 using MicroMercado.Application.DTOs.Sales;
+using MicroMercado.Domain.Models;
 
 namespace MicroMercado.Application.Services;
 
@@ -9,4 +11,10 @@ public interface IProductService
     Task<ProductSearchDTO?> GetProductByIdAsync(short productId);
 
     Task<bool> HasStockAsync(short productId, short quantity);
+    
+    Task<IEnumerable<ProductDTO>> GetAllProductsAsync();
+    Task<ProductDTO?> GetProductDetailsByIdAsync(short id);
+    Task<Product?> CreateProductAsync(CreateProductDTO productDto);
+    Task<Product?> UpdateProductAsync(UpdateProductDTO productDto);
+    Task<bool> DeleteProductAsync(short id);
 }
